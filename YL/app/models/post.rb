5ec249e5c2233f7adb0e61dbf66941f27dd4f468
Yaroslav <drummerdolbit@gmail.com>
2016-05-03 23:33:07 +0300

    work with pictures

--- YL/app/models/post.rb
+++ YL/app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ActiveRecord::Base
-	has_attached_file :image, styles:{ large: "600*600>", medium: "300*300>", thumb: "150*150#"}
+	has_attached_file :image, styles:{ large: "100x100>", medium: "300x300>", thumb: "320*180>"}
   	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end

955bba12d8597a2e76ac4d14f848ca019ff106ee
Roman Ovcharov <rozenvil13@gmail.com>
2016-05-05 01:01:32 +0300

    redcarpet + rouge

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,31 @@
+
+
 module ApplicationHelper
+
+  require 'redcarpet'
+  require 'rouge'
+  require 'rouge/plugins/redcarpet'
+
+  class HTML < Redcarpet::Render::HTML
+    include Rouge::Plugins::Redcarpet
+  end
+
+  class HTMLWithPants < Redcarpet::Render::HTML
+    include Redcarpet::Render::SmartyPants
+  end
+
+  def markdown(content)
+    @markdown ||= Redcarpet::Markdown.new(HTML, {
+      autolink:            true,
+      space_after_headers: true,
+      fenced_code_blocks:  true,
+      superscript:         true,
+      underline:           true,
+      highlight:           true,
+      footnotes:           true,
+      quote:               true,
+      tables:              true
+    })
+    @markdown.render(content).html_safe
+  end
 end

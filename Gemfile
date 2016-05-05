f4d38d0fdf3105ceec519f3c30811f0c68465211
Roman Ovcharov <rozenvil13@gmail.com>
2016-05-05 14:03:42 +0300

    babosa for russian friendly id

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ gem 'devise', '~> 3.5', '>= 3.5.6'
 gem 'friendly_id', '~> 5.1'
 gem 'redcarpet', '~> 3.3', '>= 3.3.4'
 gem 'rouge'
+gem 'babosa', '~> 1.0', '>= 1.0.2'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,17 @@
+require "babosa"
+
 class Post < ActiveRecord::Base
+
 	has_attached_file :image, styles:{ large: "100x100>", thumb: "320*320>" , back: "1000x200#"}
   	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
   	has_attached_file :background_image, styles:{ large: "100x100>", thumb: "320*180>" , back: "1000x200#"}
   	validates_attachment_content_type :background_image, content_type: /\Aimage\/.*\Z/
 
   	extend FriendlyId
- 	friendly_id :title, use: :slugged
+ 	friendly_id :title , use: :slugged
+
+ 	def normalize_friendly_id(input)
+    input.to_s.to_slug.normalize(transliterations: :russian).to_s
+  	end
   	
 end

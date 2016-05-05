cf8436e52a6caa474012b39ac397b6319d258f89
Roman Ovcharov <rozenvil13@gmail.com>
2016-05-05 22:55:42 +0300

    added pg and rails 12factor

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   # GET /posts.json
   
   def index
-    @posts = Post.paginate(:page => params[:page],per_page: 3)
+    @posts = Post.paginate(:page => params[:page],per_page: 5)
   end
 
   # GET /posts/1

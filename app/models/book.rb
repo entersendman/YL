bc1978454b9aaecb9425ee7336909e12b21ce8bd
Yaroslav <drummerdolbit@gmail.com>
2016-05-05 14:58:53 +0300

    books books

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,6 @@
 class Book < ActiveRecord::Base
 	has_attached_file :document
 	validates_attachment :document, content_type: { content_type: "application/pdf" }
-	has_attached_file :document,
-    :path => ':/home/yaroslav/Документи/YL/app/assets/documents/:id/:basename.:extension'
+	
  
 end
